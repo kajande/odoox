b4d099a4407c104be97c259c05319f7c349293be
Moctar <moctarjallo@gmail.com>
2024-12-04 20:09:50 +0000

    Impelement delete_db operation

--- odoox/db.py
+++ odoox/db.py
@@ -13,6 +13,12 @@ def execute(command, options):
             options.remove('-c')
         else:
             subprocess.run(f"docker exec -it {odoo_name} odoox db {db_name} -c".split())
+    if '-d' in options:
+        if not docker:
+            delete_db(db_name, options)
+            options.remove('-d')
+        else:
+            subprocess.run(f"docker exec -it {odoo_name} odoox db {db_name} -d".split())
 
 def create_db(db_name, options):
     host = "localhost"
@@ -33,5 +39,20 @@ def create_db(db_name, options):
             lang
         )
         print(f"Database '{db_name}' created successfully.")
+    except xmlrpc.client.Fault as e:
+        print(f"Error occurred: {e.faultString}")
+
+
+def delete_db(db_name, options):
+    host = "localhost"
+    port = 8069
+    super_admin_password = "master"
+
+    url = f"http://{host}:{port}/xmlrpc/2/db"
+    db_proxy = xmlrpc.client.ServerProxy(url)
+
+    try:
+        db_proxy.drop(super_admin_password, db_name)
+        print(f"Database '{db_name}' deleted successfully.")
     except xmlrpc.client.Fault as e:
         print(f"Error occurred: {e.faultString}")
